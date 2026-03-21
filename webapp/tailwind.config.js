131e6983c87967e0f7f195215e57ba3915cbb756
Test <test@Tests-MacBook-Pro.local>
2026-03-21 21:22:27 +0100

    Deploy: build and push

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ['"Bebas Neue"', 'cursive'],
         bebas: ['"Bebas Neue"', 'cursive'],
       },
     },
